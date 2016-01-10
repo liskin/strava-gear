a640d069ca79caac1c67b83dfcef5a69657b2faf
Tomas Janousek <tomi@nomi.cz>
2016-01-11 00:38:50 +0100

    Import Database.Esqueleto unqualified, it is prettier

--- src/Strava.hs
+++ src/Strava.hs
@@ -23,8 +23,10 @@ import Control.Monad.IO.Class (liftIO)
 import Data.List ((\\))
 import Data.List.Split (chunksOf)
 import Data.Time
-import Database.Persist
-import Database.Persist.Sqlite
+import Database.Esqueleto
+import Database.Esqueleto.Internal.Sql (veryUnsafeCoerceSqlExprValue)
+import Database.Persist (selectKeysList, selectList, selectFirst, SelectOpt(..), (<-.))
+import Database.Persist.Sqlite (runSqlite)
 import Database.Persist.TH
 import System.IO.Unsafe (unsafePerformIO)
 import Text.Printf (printf)
@@ -32,8 +34,6 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import qualified Database.Esqueleto as E
-import qualified Database.Esqueleto.Internal.Sql as E (veryUnsafeCoerceSqlExprValue)
 import qualified Strive as S
 import qualified Text.Tabular as Tab
 import qualified Text.Tabular.AsciiArt as Tab
@@ -224,7 +224,7 @@ syncHashTags new = do
             Just n -> [ActivityId <-. n]
     acts <- selectList filt []
     let actTags = [ (entityKey e, actHashTags $ entityVal e) | e <- acts ]
-        allTags = distinctOn id $ concatMap snd actTags
+        allTags = distinctUsing id $ concatMap snd actTags
     tagsRes <- syncEntities allTags
     let tagMap = Map.fromList [ (entityVal e, entityKey e)
                               | e <- keptEntities tagsRes ]
@@ -238,55 +238,55 @@ actHashTags Activity{activityName = name} =
 
 activityActivityHashTags :: [Key Activity] -> SqlPersistM [Key ActivityHashTag]
 activityActivityHashTags as =
-    fmap (map (\(E.Value v) -> v)) $ E.select $ E.from $ \ah -> do
-        E.where_ $ ah E.^. ActivityHashTagActivity `E.in_` E.valList as
-        return $ ah E.^. ActivityHashTagId
+    fmap (map (\(Value v) -> v)) $ select $ from $ \ah -> do
+        where_ $ ah ^. ActivityHashTagActivity `in_` valList as
+        return $ ah ^. ActivityHashTagId
 
 syncActivitiesComponents :: Maybe [Key Activity] -> SqlPersistM ()
 syncActivitiesComponents new = do
     l <- activitiesLongtermComponents new
     h <- activitiesHashtagComponents new
-    let dedup = distinctOn $ activityComponentActivity &&& activityComponentRole
+    let dedup = distinctUsing $ activityComponentActivity &&& activityComponentRole
     old <- mapM activityActivityComponents new
     wipeInsertMany old $ dedup $ h ++ l
 
 activityActivityComponents :: [Key Activity] -> SqlPersistM [Key ActivityComponent]
 activityActivityComponents as =
-    fmap (map (\(E.Value v) -> v)) $ E.select $ E.from $ \ac -> do
-        E.where_ $ ac E.^. ActivityComponentActivity `E.in_` E.valList as
-        return $ ac E.^. ActivityComponentId
+    fmap (map (\(Value v) -> v)) $ select $ from $ \ac -> do
+        where_ $ ac ^. ActivityComponentActivity `in_` valList as
+        return $ ac ^. ActivityComponentId
 
 activitiesLongtermComponents :: Maybe [Key Activity] -> SqlPersistM [ActivityComponent]
 activitiesLongtermComponents new = do
-    let f (_, E.Value k, E.Value c, E.Value r) = ActivityComponent k c r
-    fmap (map f) $ E.select $ E.from $ \(a `E.InnerJoin` b `E.InnerJoin` lt) -> do
-        E.on $ a E.^. ActivityGearId E.==. E.just (b E.^. BikeStravaId)
-        E.on $ b E.^. BikeId E.==. lt E.^. LongtermBikeComponentBike
-        mapM_ (\n -> E.where_ $ a E.^. ActivityId `E.in_` E.valList n) new
-        E.where_ $ lt E.^. LongtermBikeComponentStartTime E.<=. a E.^. ActivityStartTime
-        E.where_ $ lt E.^. LongtermBikeComponentEndTime E.>=. E.just (a E.^. ActivityStartTime)
-            E.||. E.isNothing (lt E.^. LongtermBikeComponentEndTime)
-        E.groupBy (a E.^. ActivityId, lt E.^. LongtermBikeComponentRole)
-        return ( castToPersistValue (E.max_ $ lt E.^. LongtermBikeComponentStartTime)
-               , a E.^. ActivityId
-               , lt E.^. LongtermBikeComponentComponent
-               , lt E.^. LongtermBikeComponentRole)
+    let f (_, Value k, Value c, Value r) = ActivityComponent k c r
+    fmap (map f) $ select $ from $ \(a `InnerJoin` b `InnerJoin` lt) -> do
+        on $ a ^. ActivityGearId ==. just (b ^. BikeStravaId)
+        on $ b ^. BikeId ==. lt ^. LongtermBikeComponentBike
+        mapM_ (\n -> where_ $ a ^. ActivityId `in_` valList n) new
+        where_ $ lt ^. LongtermBikeComponentStartTime <=. a ^. ActivityStartTime
+        where_ $ lt ^. LongtermBikeComponentEndTime >=. just (a ^. ActivityStartTime)
+            ||. isNothing (lt ^. LongtermBikeComponentEndTime)
+        groupBy (a ^. ActivityId, lt ^. LongtermBikeComponentRole)
+        return ( castToPersistValue (max_ $ lt ^. LongtermBikeComponentStartTime)
+               , a ^. ActivityId
+               , lt ^. LongtermBikeComponentComponent
+               , lt ^. LongtermBikeComponentRole)
 
 activitiesHashtagComponents :: Maybe [Key Activity] -> SqlPersistM [ActivityComponent]
 activitiesHashtagComponents new = do
-    let f (_, E.Value k, E.Value c, E.Value r) = ActivityComponent k c r
-    fmap (map f) $ E.select $ E.from $ \(a `E.InnerJoin` ah `E.InnerJoin` h) -> do
-        E.on $ a E.^. ActivityId E.==. ah E.^. ActivityHashTagActivity
-        E.on $ ah E.^. ActivityHashTagTag E.==. h E.^. HashTagBikeComponentTag
-        mapM_ (\n -> E.where_ $ a E.^. ActivityId `E.in_` E.valList n) new
-        E.where_ $ h E.^. HashTagBikeComponentStartTime E.<=. a E.^. ActivityStartTime
-        E.where_ $ h E.^. HashTagBikeComponentEndTime E.>=. E.just (a E.^. ActivityStartTime)
-            E.||. E.isNothing (h E.^. HashTagBikeComponentEndTime)
-        E.groupBy (a E.^. ActivityId, h E.^. HashTagBikeComponentRole)
-        return ( castToPersistValue (E.max_ $ h E.^. HashTagBikeComponentStartTime)
-               , a E.^. ActivityId
-               , h E.^. HashTagBikeComponentComponent
-               , h E.^. HashTagBikeComponentRole)
+    let f (_, Value k, Value c, Value r) = ActivityComponent k c r
+    fmap (map f) $ select $ from $ \(a `InnerJoin` ah `InnerJoin` h) -> do
+        on $ a ^. ActivityId ==. ah ^. ActivityHashTagActivity
+        on $ ah ^. ActivityHashTagTag ==. h ^. HashTagBikeComponentTag
+        mapM_ (\n -> where_ $ a ^. ActivityId `in_` valList n) new
+        where_ $ h ^. HashTagBikeComponentStartTime <=. a ^. ActivityStartTime
+        where_ $ h ^. HashTagBikeComponentEndTime >=. just (a ^. ActivityStartTime)
+            ||. isNothing (h ^. HashTagBikeComponentEndTime)
+        groupBy (a ^. ActivityId, h ^. HashTagBikeComponentRole)
+        return ( castToPersistValue (max_ $ h ^. HashTagBikeComponentStartTime)
+               , a ^. ActivityId
+               , h ^. HashTagBikeComponentComponent
+               , h ^. HashTagBikeComponentRole)
 
 
 -- Report --
@@ -300,25 +300,25 @@ report fileName = do
 
 componentReport :: SqlPersistM (Tab.Table String String String)
 componentReport = do
-    res <- E.select $
-        E.from $ \(c `E.InnerJoin` ac `E.InnerJoin` a `E.InnerJoin` r) -> do
-            E.on (c E.^. ComponentId E.==. ac E.^. ActivityComponentComponent)
-            E.on (ac E.^. ActivityComponentActivity E.==. a E.^. ActivityId)
-            E.on (ac E.^. ActivityComponentRole E.==. r E.^. ComponentRoleId)
-            E.groupBy (c E.^. ComponentId)
-            E.orderBy
-                [ E.asc $ r E.^. ComponentRoleName
-                , E.asc $ c E.^. ComponentUniqueId ]
-            let iniTime = E.just $ c E.^. ComponentInitialSeconds
-                iniDist = E.just $ c E.^. ComponentInitialMeters
-                sumMovingTime = E.sum_ $ a E.^. ActivityMovingTime
-                sumDist = E.sum_ $ a E.^. ActivityDistance
-                firstUsage = E.min_ $ a E.^. ActivityStartTime
-                lastUsage = E.max_ $ a E.^. ActivityStartTime
+    res <- select $
+        from $ \(c `InnerJoin` ac `InnerJoin` a `InnerJoin` r) -> do
+            on (c ^. ComponentId ==. ac ^. ActivityComponentComponent)
+            on (ac ^. ActivityComponentActivity ==. a ^. ActivityId)
+            on (ac ^. ActivityComponentRole ==. r ^. ComponentRoleId)
+            groupBy (c ^. ComponentId)
+            orderBy
+                [ asc $ r ^. ComponentRoleName
+                , asc $ c ^. ComponentUniqueId ]
+            let iniTime = just $ c ^. ComponentInitialSeconds
+                iniDist = just $ c ^. ComponentInitialMeters
+                sumMovingTime = sum_ $ a ^. ActivityMovingTime
+                sumDist = sum_ $ a ^. ActivityDistance
+                firstUsage = min_ $ a ^. ActivityStartTime
+                lastUsage = max_ $ a ^. ActivityStartTime
             return
                 ( r, c
-                , iniTime E.+. sumMovingTime
-                , iniDist E.+. sumDist
+                , iniTime +. sumMovingTime
+                , iniDist +. sumDist
                 , firstUsage, lastUsage )
     let ids = [ T.unpack $ componentRoleName r
               | (Entity _ r, _, _, _, _, _) <- res ]
@@ -332,8 +332,8 @@ componentReport = do
         tab = [ [ T.unpack $ componentUniqueId c, T.unpack $ componentName c
                 , timeFormat firstUsage, timeFormat lastUsage, niceTime, niceDist ]
               | ( _, Entity _ c
-                , E.Value (Just time), E.Value (Just dist)
-                , E.Value (Just firstUsage), E.Value (Just lastUsage)
+                , Value (Just time), Value (Just dist)
+                , Value (Just firstUsage), Value (Just lastUsage)
                 ) <- res
               , let niceTime = printf "%.1f" ((fromIntegral time :: Double) / 3600) ++ " hours"
               , let niceDist = printf "%.0f" ((dist :: Double) / 1000) ++ " km" ]
@@ -354,7 +354,7 @@ syncConfig conf = do
     roles <- syncEntitiesDel
         [ ComponentRole n | ConfRole n <- cs ]
     bikes <- selectList [] []
-    let allTags = distinctOn id [ HashTag t | ConfHashTag t _ _ _ _ <- cs ]
+    let allTags = distinctUsing id [ HashTag t | ConfHashTag t _ _ _ _ <- cs ]
     tags <- syncEntities allTags
     let componentMap = Map.fromList
             [ (componentUniqueId v, k) | Entity k v <- keptEntities components ]
@@ -426,8 +426,8 @@ parseUTCTime' t =
 
 -- Helpers --
 
-distinctOn :: (Ord b) => (a -> b) -> [a] -> [a]
-distinctOn f = go Set.empty
+distinctUsing :: (Ord b) => (a -> b) -> [a] -> [a]
+distinctUsing f = go Set.empty
     where
         go _ [] = []
         go seen (x:xs) =
@@ -468,7 +468,7 @@ delEntities :: (PersistEntity rec, PersistEntityBackend rec ~ SqlBackend)
 delEntities res = do
     allKeys <- selectKeysList [] []
     let delKeys = allKeys \\ map entityKey (keptEntities res)
-    mapM (\k -> delete k >> return (UpsertDeleted k)) delKeys
+    mapM (\k -> deleteKey k >> return (UpsertDeleted k)) delKeys
 
 keptEntities :: [UpsertResult rec] -> [Entity rec]
 keptEntities = concatMap $ \case
@@ -501,9 +501,9 @@ wipeInsertMany :: forall rec.
                   (PersistEntity rec, PersistEntityBackend rec ~ SqlBackend)
                => Maybe [Key rec] -> [rec] -> SqlPersistM ()
 wipeInsertMany old recs = do
-    E.delete $ E.from $ \row -> case old of
+    delete $ from $ \row -> case old of
         Nothing -> return ()
-        Just olds -> E.where_ $ row E.^. persistIdField `E.in_` E.valList olds
+        Just olds -> where_ $ row ^. persistIdField `in_` valList olds
     -- FIXME: https://github.com/yesodweb/persistent/issues/527
     mapM_ insertMany_ (chunksOf 100 recs)
 
@@ -514,5 +514,5 @@ m ! v = unsafePerformIO $ try (evaluate (m Map.! v)) >>= \case
         error $ show m ++ " ! " ++ show v ++ ": " ++ show e
 
 -- | Avoids the performance penalty of parsing into UTCTime
-castToPersistValue :: E.SqlExpr (E.Value a) -> E.SqlExpr (E.Value PersistValue)
-castToPersistValue = E.veryUnsafeCoerceSqlExprValue
+castToPersistValue :: SqlExpr (Value a) -> SqlExpr (Value PersistValue)
+castToPersistValue = veryUnsafeCoerceSqlExprValue
