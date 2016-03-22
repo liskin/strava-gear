66d30a33dea6a8fa1646394c420f587e505cf8fc
Tomas Janousek <tomi@nomi.cz>
2016-03-22 23:20:15 +0100

    Add another report (bikes with current components)

--- src/Strava.hs
+++ src/Strava.hs
@@ -294,8 +294,12 @@ activitiesHashtagComponents new = do
 report :: T.Text -> IO ()
 report fileName = do
     runSqlite fileName $ do
-        tab <- componentReport
-        liftIO $ putStr $ Tab.render id id id tab
+        tab1 <- componentReport
+        tab2 <- bikesReport
+        liftIO $ do
+            putStr $ Tab.render id id id tab1
+            putStrLn ""
+            putStr $ Tab.render id id id tab2
         return ()
 
 componentReport :: SqlPersistM (Tab.Table String String String)
@@ -335,6 +339,46 @@ componentReport = do
                 , Value (Just time), Value (Just dist)
                 , Value (Just firstUsage), Value (Just lastUsage)
                 ) <- res
+              , let niceTime = printf "%.1f" ((fromIntegral time :: Double) / 3600) ++ " hours"
+              , let niceDist = printf "%.0f" ((dist :: Double) / 1000) ++ " km" ]
+    return $ Tab.Table rh ch tab
+
+bikesReport :: SqlPersistM (Tab.Table String String String)
+bikesReport = do
+    res <- select $ from $ \(b `InnerJoin` lt `InnerJoin` r `InnerJoin` c) -> do
+        on $ b ^. BikeId ==. lt ^. LongtermBikeComponentBike
+        on $ lt ^. LongtermBikeComponentRole ==. r ^. ComponentRoleId
+        on $ lt ^. LongtermBikeComponentComponent ==. c ^. ComponentId
+        groupBy (b ^. BikeId, r ^. ComponentRoleId)
+        orderBy
+            [ asc $ b ^. BikeName
+            , asc $ r ^. ComponentRoleName
+            , asc $ c ^. ComponentUniqueId ]
+        let iniTime = just $ c ^. ComponentInitialSeconds
+            iniDist = just $ c ^. ComponentInitialMeters
+        let sub :: (PersistField a) => (SqlExpr (Entity Activity) -> SqlExpr (Value a)) -> SqlExpr (Value a)
+            sub f = sub_select $ from $ \(a `InnerJoin` ac) -> do
+                on $ ac ^. ActivityComponentActivity ==. a ^. ActivityId
+                where_ $ c ^. ComponentId ==. ac ^. ActivityComponentComponent
+                return $ f a
+            sumMovingTime = sub $ \a -> sum_ $ a ^. ActivityMovingTime
+            sumDist = sub $ \a -> sum_ $ a ^. ActivityDistance
+        return
+            ( castToPersistValue (max_ $ lt ^. LongtermBikeComponentStartTime)
+            , b, r, c
+            , iniTime +. sumMovingTime
+            , iniDist +. sumDist
+            )
+    let rh = Tab.Group Tab.NoLine $ map Tab.Header $
+            [ T.unpack $ bikeName b
+            | (_, Entity _ b, _, _, _, _) <- res ]
+        ch = Tab.Group Tab.DoubleLine
+            [ Tab.Group Tab.SingleLine [Tab.Header "role", Tab.Header "id", Tab.Header "name"]
+            , Tab.Group Tab.SingleLine [Tab.Header "time", Tab.Header "distance"]
+            ]
+        tab = [ [ T.unpack $ componentRoleName r, T.unpack $ componentUniqueId c,
+                  T.unpack $ componentName c, niceTime, niceDist ]
+              | (_, _, Entity _ r, Entity _ c, Value (Just time), Value (Just dist)) <- res
               , let niceTime = printf "%.1f" ((fromIntegral time :: Double) / 3600) ++ " hours"
               , let niceDist = printf "%.0f" ((dist :: Double) / 1000) ++ " km" ]
     return $ Tab.Table rh ch tab
