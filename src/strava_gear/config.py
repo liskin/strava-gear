6990590bce0c9c436d6aa1c425043a44df1a263a
Tomas Janousek <tomi@nomi.cz>
2021-09-04 23:57:40 +0100

    Process config into dataclasses

--- src/strava_gear/config.py
+++ src/strava_gear/config.py
@@ -1,6 +1,11 @@
+from dataclasses import dataclass
 import datetime
+from typing import Dict
+from typing import List
+from typing import Optional
 
 import jsonschema  # type: ignore [import]
+import pandas as pd  # type: ignore [import]
 import yaml
 
 config_format_checker = jsonschema.FormatChecker()
@@ -58,7 +63,56 @@ def format_checker_hashtag(s: str):
     return s.startswith('#')
 
 
-def read_config(inp):
+@dataclass(frozen=True)
+class Component:
+    ident: str  # TODO: do we need this?
+    name: str
+    distance: int = 0  # TODO: pint
+    hours: int = 0
+
+
+def process_component(k: str, v) -> Component:
+    if v is None:
+        return Component(ident=k, name=k)
+    elif isinstance(v, str):
+        return Component(ident=k, name=v)
+    else:
+        name = v.pop('name', k)
+        return Component(ident=k, name=name, **v)  # TODO: parse distance/hours
+
+
+@dataclass(frozen=True)
+class Rule:
+    bikes: Dict[str, Dict[str, str]]
+    since: pd.Timestamp = pd.to_datetime(0, utc=True)
+    hashtag: Optional[str] = None
+
+
+def process_rule(r: Dict) -> Rule:
+    kwargs = {}
+    if 'since' in r:
+        kwargs['since'] = pd.to_datetime(r.pop('since'), utc=True)
+    if 'hashtag' in r:
+        kwargs['hashtag'] = r.pop('hashtag')
+    return Rule(bikes=r, **kwargs)
+
+
+@dataclass(frozen=True)
+class Config:
+    aliases: Dict[str, str]
+    components: Dict[str, Component]
+    rules: List[Rule]
+
+
+def process_config(c: Dict) -> Config:
+    return Config(
+        aliases=c.get('aliases', []),
+        components={k: process_component(k, v) for k, v in c.get('components', {}).items()},
+        rules=[process_rule(r) for r in c.get('rules', [])],
+    )
+
+
+def read_config(inp) -> Config:
     c = yaml.safe_load(inp)
     jsonschema.validate(c, config_schema, format_checker=config_format_checker)
-    return c
+    return process_config(c)
