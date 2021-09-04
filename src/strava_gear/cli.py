49651c40a934f3cae620183f008d53f9b978ee36
Tomas Janousek <tomi@nomi.cz>
2021-09-04 21:17:05 +0100

    Load input CSV

--- src/strava_gear/cli.py
+++ src/strava_gear/cli.py
@@ -1,6 +1,27 @@
+import pandas as pd  # type: ignore [import]
 import click
 
 
+def read_input_csv(inp):
+    """
+    Loads activities from CSV generated from this command:
+
+        sqlite3 ~/.local/share/strava_offline/strava.sqlite \
+            ".mode csv" \
+            ".headers on" \
+            "SELECT gear_id, start_date, moving_time, distance FROM activity" \
+            >activities.csv
+    """
+    return pd.read_csv(
+        inp,
+        usecols=['gear_id', 'start_date', 'moving_time', 'distance'],
+        dtype={'gear_id': 'string', 'moving_time': 'int64', 'distance': 'float64'},
+        parse_dates=['start_date'],
+    )
+
+
 @click.command()
-def main():
-    pass
+@click.argument('input', type=click.File('r'), default='-')
+def main(input):
+    i = read_input_csv(input)
+    print(i)
