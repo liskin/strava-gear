dd631daacdd183360d5fea4bb5ad0f8cd5cc7bbc
Tomas Janousek <tomi@nomi.cz>
2021-09-06 00:07:37 +0100

    Implement Rule addition

--- src/strava_gear/config.py
+++ src/strava_gear/config.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
+from dataclasses import field
+from dataclasses import replace
 import datetime
 from typing import Dict
 from typing import List
+from typing import Set
 
 import jsonschema  # type: ignore [import]
 import pandas as pd  # type: ignore [import]
@@ -76,9 +79,43 @@ def process_component(k: str, v) -> Component:
 
 @dataclass(frozen=True)
 class Rule:
-    bikes: Dict[str, Dict[str, str]]
-    hashtags: Dict[str, Dict[str, str]]
+    bikes: Dict[str, Dict[str, str]] = field(default_factory=dict)
+    hashtags: Dict[str, Dict[str, str]] = field(default_factory=dict)
     since: pd.Timestamp = pd.to_datetime(0, utc=True)
+
+    def __add__(self, other):
+        """
+        Combine two rules. The second rule's since must be later than the first. Component mappings
+        in the second rule then override those in the first. Additionally, components newly assigned
+        to another bike are automatically removed from the old one.
+        """
+        if not isinstance(other, Rule):
+            return NotImplemented
+        if other.since < self.since:
+            return NotImplemented
+
+        other_components = {c for m in other.bikes.values() for c in m.values()}
+        bikes = prune_mapping(update_mappings(filter_mapping(self.bikes, other_components), other.bikes))
+        hashtags = prune_mapping(update_mappings(self.hashtags, other.hashtags))
+        return replace(other, bikes=bikes, hashtags=hashtags)
+
+
+def prune_mapping(m: Dict[str, Dict[str, str]]) -> Dict[str, Dict[str, str]]:
+    """Prune mapping—drop null/None components and empty dicts."""
+    return {a: b for a, b in ((a, {c: d for c, d in b.items() if d}) for a, b in m.items()) if b}
+
+
+def update_mappings(
+    m1: Dict[str, Dict[str, str]],
+    m2: Dict[str, Dict[str, str]]
+) -> Dict[str, Dict[str, str]]:
+    """Override component mappings in m1 by those in m2."""
+    return {a: {**m1.get(a, {}), **m2.get(a, {})} for a in m1.keys() | m2.keys()}
+
+
+def filter_mapping(m: Dict[str, Dict[str, str]], f: Set[str]) -> Dict[str, Dict[str, str]]:
+    """Remove components from a mapping."""
+    return {a: {c: d for c, d in b.items() if d not in f} for a, b in m.items()}
 
 
 def process_rule(r: Dict) -> Rule:
---
+++ tests/test_config.py
@@ -0,0 +1,53 @@
+import pandas as pd  # type: ignore [import]
+import pytest  # type: ignore [import]
+
+from strava_gear.config import Rule
+
+
+def test_rule():
+    assert Rule() + Rule() == Rule()
+
+    # since taken from right
+    assert Rule(since=pd.to_datetime('2020-01-01')) + Rule(since=pd.to_datetime('2020-01-02T01:00')) == \
+        Rule(since=pd.to_datetime('2020-01-02T01:00'))
+    with pytest.raises(TypeError):
+        assert Rule(since=pd.to_datetime('2020-01-02')) + Rule(since=pd.to_datetime('2020-01-01T01:00')) == \
+            Rule(since=pd.to_datetime('2020-01-01T01:00'))
+
+    # neutral value on either side
+    assert Rule(bikes={'b1': {'c': 'c1'}}) + Rule() == Rule(bikes={'b1': {'c': 'c1'}})
+    assert Rule() + Rule(bikes={'b1': {'c': 'c1'}}) == Rule(bikes={'b1': {'c': 'c1'}})
+    assert Rule(hashtags={'#b1': {'c': 'c1'}}) + Rule() == Rule(hashtags={'#b1': {'c': 'c1'}})
+    assert Rule() + Rule(hashtags={'#b1': {'c': 'c1'}}) == Rule(hashtags={'#b1': {'c': 'c1'}})
+
+    # pruning
+    assert Rule(bikes={'b1': {}}) + Rule(bikes={}) == Rule(bikes={})
+    assert Rule(bikes={}) + Rule(bikes={'b1': {}}) == Rule(bikes={})
+    assert Rule(bikes={'b1': {'c': 'c1'}}) + Rule(bikes={'b1': {'c': None}}) == Rule(bikes={})
+    assert Rule(hashtags={'#b1': {}}) + Rule(hashtags={}) == Rule(hashtags={})
+    assert Rule(hashtags={}) + Rule(hashtags={'#b1': {}}) == Rule(hashtags={})
+    assert Rule(hashtags={'#b1': {'c': 'c1'}}) + Rule(hashtags={'#b1': {'c': None}}) == Rule(hashtags={})
+
+    # merging
+    assert Rule(bikes={'b1': {'c': 'c1'}}) + Rule(bikes={'b1': {'d': 'd1'}}) == \
+        Rule(bikes={'b1': {'c': 'c1', 'd': 'd1'}})
+    assert Rule(bikes={'b1': {'c': 'c1'}}) + Rule(bikes={'b2': {'d': 'c2'}}) == \
+        Rule(bikes={'b1': {'c': 'c1'}, 'b2': {'d': 'c2'}})
+    assert Rule(hashtags={'#b1': {'c': 'c1'}}) + Rule(hashtags={'#b1': {'d': 'd1'}}) == \
+        Rule(hashtags={'#b1': {'c': 'c1', 'd': 'd1'}})
+    assert Rule(hashtags={'#b1': {'c': 'c1'}}) + Rule(hashtags={'#b2': {'d': 'c2'}}) == \
+        Rule(hashtags={'#b1': {'c': 'c1'}, '#b2': {'d': 'c2'}})
+
+    # overriding
+    assert Rule(bikes={'b1': {'c': 'c1'}}) + Rule(bikes={'b1': {'c': 'c2'}}) == \
+        Rule(bikes={'b1': {'c': 'c2'}})
+    assert Rule(hashtags={'#b1': {'c': 'c1'}}) + Rule(hashtags={'#b1': {'c': 'c2'}}) == \
+        Rule(hashtags={'#b1': {'c': 'c2'}})
+
+    # moving a component from one bike to another…
+    assert Rule(bikes={'b1': {'c': 'c1', 'd': 'd1'}}) + Rule(bikes={'b2': {'c': 'c1'}}) == \
+        Rule(bikes={'b1': {'d': 'd1'}, 'b2': {'c': 'c1'}})
+
+    # … hashtags aren't exclusive though
+    assert Rule(hashtags={'#b1': {'c': 'c1', 'd': 'd1'}}) + Rule(hashtags={'#b2': {'c': 'c1'}}) == \
+        Rule(hashtags={'#b1': {'c': 'c1', 'd': 'd1'}, '#b2': {'c': 'c1'}})
