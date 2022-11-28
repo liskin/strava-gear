a26915be8c7e5e99cd53fe7950fcfda19e502589
Tomas Janousek <tomi@nomi.cz>
2022-11-28 15:14:08 +0000

    Avoid runtime setuptools dependency
    Using `pkg_resources` meant that setuptools had to be available during
    runtime, but this dependency was undeclared. Switch to
    importlib.metadata which is in stdlib since Python 3.8.

--- src/strava_gear/__init__.py
+++ src/strava_gear/__init__.py
@@ -1,8 +1,14 @@
-from pkg_resources import DistributionNotFound
-from pkg_resources import get_distribution
+# type: ignore
+
+import sys
+
+if sys.version_info >= (3, 10):
+    import importlib.metadata as importlib_metadata  # isort: skip
+else:
+    import importlib_metadata  # isort: skip
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = importlib_metadata.version(__name__)
+except importlib_metadata.PackageNotFoundError:
     # package is not installed
     pass
